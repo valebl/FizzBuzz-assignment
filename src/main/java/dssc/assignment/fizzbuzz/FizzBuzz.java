69ba5cbd6075c9248ff9e52f15e58311b8a7b665
Dario Campagna <campagna@esteco.com>
2020-10-22 15:53:08 +0200

    We now convert 1 to the string 1

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -6,4 +6,7 @@ public class FizzBuzz {
         
     }
 
+    public String convert(int number) {
+        return "1";
+    }
 }
---
+++ src/test/java/dssc/assignment/fizzbuzz/ConvertNumbersToStrings.java
@@ -0,0 +1,14 @@
+package dssc.assignment.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConvertNumbersToStrings {
+
+    @Test
+    void number_1() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.convert(1));
+    }
+}
