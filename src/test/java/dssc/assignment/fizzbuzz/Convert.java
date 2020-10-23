f9dd6218444bb231e521c928816bbd520eba3b41
Dario Campagna <campagna@esteco.com>
2020-10-23 15:04:53 +0200

    Refactor tests

--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -1,8 +1,8 @@
 package dssc.assignment.fizzbuzz;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,21 +15,10 @@ public class Convert {
         assertEquals(expected, fizzBuzz.convert(number));
     }
 
-    @Test
-    void number_3_to_Fizz() {
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 24})
+    void multiples_of_3_to_Fizz(int number) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("Fizz", fizzBuzz.convert(3));
-    }
-
-    @Test
-    void number_6_to_Fizz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("Fizz", fizzBuzz.convert(6));
-    }
-
-    @Test
-    void number_24_to_Fizz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("Fizz", fizzBuzz.convert(24));
+        assertEquals("Fizz", fizzBuzz.convert(number));
     }
 }
