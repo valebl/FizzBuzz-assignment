d82219591c0cb33bc204e8ab8ab7c8e08e75dad5
Dario Campagna <campagna@esteco.com>
2020-10-23 14:38:16 +0200

    We now convert 3 to Fizz

--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -1,5 +1,6 @@
 package dssc.assignment.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,5 +13,11 @@ public class Convert {
     void numbers_not_divisible_by_3_or_5_to_strings(int number, String expected) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals(expected, fizzBuzz.convert(number));
+    }
+
+    @Test
+    void number_3_to_Fizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.convert(3));
     }
 }
