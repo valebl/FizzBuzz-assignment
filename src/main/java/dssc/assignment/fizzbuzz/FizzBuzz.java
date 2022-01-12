f637cf56f3512a4899288b0aa08b29873a416fbf
valebl <kiwi5.vb@gmail.com>
2022-01-12 10:02:28 +0100

    Test convert 6 to Fizz: pass

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -9,7 +9,10 @@ public class FizzBuzz {
     public String convert(int number) {
         if (number == 3) {
             return "Fizz";
+        } else if (number == 6) {
+            return "Fizz";
+        } else {
+            return Integer.toString(number);
         }
-       return Integer.toString(number);
     }
 }
