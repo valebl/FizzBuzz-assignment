4ccec4c53729cb563dfff3447197638036195b6c
valebl <kiwi5.vb@gmail.com>
2022-01-12 10:04:38 +0100

    Test convert 24 to Fizz: pass

--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -27,6 +27,12 @@ public class Convert {
         assertEquals("Fizz", fizzBuzz.convert(6));
     }
 
+    @Test
+    void number_24_to_Fizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.convert(24));
+    }
+
 }
 
     /*@Test
