2667a0c8d181c28ce078e036be4b6cdd9ec7970a
valebl <kiwi5.vb@gmail.com>
2022-01-12 10:22:01 +0100

    All tests pass

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -7,7 +7,11 @@ public class FizzBuzz {
     }
 
     public String convert(int number) {
-        if (number % 3 == 0) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else if (number % 3 == 0) {
             return "Fizz";
         } else {
             return Integer.toString(number);
--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Convert {
 
     @ParameterizedTest
-    @CsvSource({"1, 1", "2, 2", "40, 40"})
+    @CsvSource({"1, 1", "2, 2", "41, 41"})
     void numbers_not_divisible_by_3_or_5_to_strings(int number, String expected) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals(expected, fizzBuzz.convert(number));
@@ -22,6 +22,23 @@ public class Convert {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.convert(number));
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 100})
+    void multiples_of_5_to_Buzz(int number) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Buzz", fizzBuzz.convert(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {15, 45})
+    void multiples_of_3_and_5_to_FizzBuzz(int number) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("FizzBuzz", fizzBuzz.convert(number));
+    }
+
+
+
 }
 
     /*@Test
